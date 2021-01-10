9fa01393142df4b5374c5fb3048364bc112569bd
Navaneeth K M <34748927+navaneethkm004@users.noreply.github.com>
2021-01-10 13:48:35 +0530

    First commit

---
+++ main.py
@@ -0,0 +1,41 @@
+import requests, replitdb, asyncio, flask, threading
+db = replitdb.AsyncClient()
+async def ping():
+	lst = []
+	pings = str(await db.view('pings')).split('\n')
+	for i in pings:
+		if i not in lst:
+			try:
+				if i != '':requests.get(i)
+			except:
+				continue
+			lst.append(i)
+
+async def loop():
+	await ping()
+	await asyncio.sleep(5)
+
+asyncio.ensure_future(loop())
+
+
+app = flask.Flask(__name__)
+@app.route('/')
+def contact():
+	return flask.render_template('add.html')
+  
+@app.route('/add', methods=['POST'])
+def send():
+	newPing = flask.request.form['add']
+	pings = str(asyncio.run(db.view('pings'))).split('\n')
+	if newPing not in pings:
+		try:
+			requests.get(newPing)
+			asyncio.run(db.set(pings=str(asyncio.run(db.view('pings'))) + '\n' + newPing))
+			return flask.render_template('success.html')
+		except:
+			return flask.render_template('incorrect.html')
+	else:
+		return flask.render_template('duplicate.html')
+
+
+app.run(host='0.0.0.0', port=8080)
